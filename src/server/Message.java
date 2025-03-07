3e5bf2a1cb9522106fb8f950f3a4b5433e5f9e65
30208300 <colby.campbell@ucalgary.ca>
2025-03-07 14:17:40 -0700

    Created a Message class to facilitate communication between threads in the server

---
+++ src/server/Message.java
@@ -0,0 +1,36 @@
+// Make a package?
+
+/**
+ * This class will be used as a basis for communication between threads
+ */
+public class Message {
+    private final long recipient; // The thread ID?
+    private final String content;
+
+    /**
+     * Constructor for the Message class
+     * @param recipient The thread you want to send information to
+     * @param content The information that you want to send to that thread
+     */
+    public Message(Thread recipient, String content) {
+        this.recipient = recipient.getId();
+        this.content = content;
+    }
+
+    /**
+     * Allows a thread to quickly check if they are the recipent
+     * @param currentThread Should be the thread calling this function
+     * @return boolean of whether or not they are the designated recipent
+     */
+    public boolean isRecipient(Thread currentThread) {
+        return currentThread.threadId() == recipient;
+    }
+
+    /**
+     * Get the content of the message
+     * @return a String that is the content of the message
+     */
+    public String getContent() {
+        return content;
+    }
+}
