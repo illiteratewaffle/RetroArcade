10e6943a3376779dd77f82085aabed4f938eebaa
10078303 <jeff.suen@ucalgary.ca>
2025-03-13 15:13:45 -0600

    Created a C4Main main file to compile code. Code compiles with no crashes

--- src/GameLogic/Connect4/C4Controller.java
+++ src/GameLogic/Connect4/C4Controller.java
@@ -4,6 +4,7 @@ public class C4Controller {
     private C4GameLogic c4GameLogic;
     void start() {
         c4GameLogic = new C4GameLogic();
+        System.out.println("A new game of connect four has started");
     }
     void handleInput() {
     }
---
+++ src/GameLogic/Connect4/C4Main.java
@@ -0,0 +1,8 @@
+package gamelogic.Connect4;
+
+public class C4Main {
+    public static void main(String[] args) {
+        C4Controller c4Controller = new C4Controller();
+        c4Controller.start();
+    }
+}
