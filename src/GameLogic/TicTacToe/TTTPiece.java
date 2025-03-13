d2755284a8fe8c8d615bc6ca8375fa701d3c9595
Emma_Djukic <emma.djukic@ucalgary.ca>
2025-03-12 21:11:14 -0600

    GameController class for TTT added. Piece class started.

---
+++ src/GameLogic/TicTacToe/TTTPiece.java
@@ -0,0 +1,29 @@
+package GameLogic;
+
+/**
+ * Enum representing the possible pieces in a Tic-Tac-Toe game.
+ *
+ * Author: Emma Djukic ~ Game Logic
+ */
+public enum TTTPiece {
+    EMPTY(0),  // No piece placed
+    X(1),      // X piece
+    O(2);      // O piece
+
+    private final int value;
+
+    TTTPiece(int value) {
+        this.value = value;
+    }
+
+    /**
+     * Returns the integer value of the piece.
+     * @return 0 for EMPTY, 1 for X, 2 for O
+     */
+    public int getValue() {
+        return value;
+    }
+
+
+    }
+}
