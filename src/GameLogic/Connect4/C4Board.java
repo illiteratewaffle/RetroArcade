3a485d008597f6204a37a907eb76f6bb93ddd8b7
10078303 <jeff.suen@ucalgary.ca>
2025-03-13 22:11:56 -0600

    switched a print statement

--- src/GameLogic/Connect4/C4Board.java
+++ src/GameLogic/Connect4/C4Board.java
@@ -5,6 +5,7 @@ public class C4Board {
 
     public C4Board(int rows, int col) {
         c4Board = new C4Piece[rows][col];
+        initC4BlankBoard(); // fills board with BLANK pieces
     }
 
     void setC4Piece() {
@@ -12,13 +13,40 @@ public class C4Board {
     void getC4Piece() {
     }
     C4Piece[][] getC4Board() {
-        return new C4Piece[0][0];
+        return c4Board;
     }
     void initC4BlankBoard() {
+        for (int i = 0; i < c4Board.length; i++) {
+            for (int j = 0; j < c4Board[i].length; j++) {
+                c4Board[i][j] = C4Piece.BLANK; // Initialize all cells as BLANK
+            }
+        }
     }
 
     @Override
     public String toString() {
-        return "";
+        StringBuilder sb = new StringBuilder();
+
+        // Construct 2D board grid representation in the console
+        for (int i = 0; i < c4Board.length; i++) {
+            for (int j = 0; j < c4Board[i].length; j++) {
+                String pieceRepresentation;
+
+                if (c4Board[i][j] == null || c4Board[i][j] == C4Piece.BLANK) {
+                    pieceRepresentation = "_";  // Empty slot
+                } else if (c4Board[i][j] == C4Piece.RED) {
+                    pieceRepresentation = "R";  // Red piece
+                } else if (c4Board[i][j] == C4Piece.BLUE) {
+                    pieceRepresentation = "B";  // Blue piece
+                } else {
+                    pieceRepresentation = "?";  // Fallback in case of an unknown piece type
+                }
+
+                sb.append(pieceRepresentation);
+                sb.append(" "); // Space between pieces for readability
+            }
+            sb.append("\n"); // New line after each row
+        }
+        return sb.toString();
     }
 }
--- src/GameLogic/Connect4/C4Controller.java
+++ src/GameLogic/Connect4/C4Controller.java
@@ -3,9 +3,13 @@ package gamelogic.Connect4;
 public class C4Controller {
     private C4GameLogic c4GameLogic;
     void start() {
-        c4GameLogic = new C4GameLogic();
         System.out.println("A new game of connect four has started");
+        c4GameLogic = new C4GameLogic();
+        printBoard();
     }
     void handleInput() {
+    }
+    void printBoard() {
+        System.out.println(c4GameLogic);
     }
 }
