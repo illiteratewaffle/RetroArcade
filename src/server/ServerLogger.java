e877418be4f5bb20e8b538cfcaa8ce3d07c83c21
30208300 <colby.campbell@ucalgary.ca>
2025-03-14 00:57:15 -0600

    Added daily log switching and a logs directory so the log file name is based on the day.

--- src/server/ServerLogger.java
+++ src/server/ServerLogger.java
@@ -3,6 +3,9 @@ package server;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -10,18 +13,43 @@ import java.time.format.DateTimeFormatter;
  * This class will be responsible for logging all information
  */
 public class ServerLogger {
+    private static final String DIR = "logs";
     private static PrintWriter writer;
+    private static String filename = "logs_" + ZonedDateTime.now(java.time.ZoneId.systemDefault()).format(DateTimeFormatter.ISO_LOCAL_DATE);
 
     // This could be contained in a method that starts the PrintWriter if preferred later
     static {
+        createPrintWriter();
+    }
+
+    /**
+     * Creates a PrintWriter object
+     */
+    private static void createPrintWriter() {
         try {
+            // Make sure the log file has the necessary directory
+            Files.createDirectories(Paths.get(DIR));
             // Create the writer object
-            writer = new PrintWriter(new FileWriter("log.txt", true));
+            writer = new PrintWriter(new FileWriter(DIR + "\\" + filename, true));
             // Ensures the log file is saved properly if the server crashes or closed incorrectly
             Runtime.getRuntime().addShutdownHook(new Thread(ServerLogger::close));
         } catch (IOException e) {
             // This should be changed later to be more robust
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * Updates the filename if different and creates a new PrintWriter object
+     */
+    private static void updateFilename() {
+        // Get the new filename
+        String updatedFilename = "logs_" + ZonedDateTime.now(java.time.ZoneId.systemDefault()).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        // If the filenames do not match, update the filename, close the old log file and create a new one
+        if (!filename.equals(updatedFilename)) {
+            filename = updatedFilename;
+            close();
+            createPrintWriter();
         }
     }
 
