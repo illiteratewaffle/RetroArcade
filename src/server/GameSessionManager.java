4061e63db4bfd03533006ca36d48ae710907e870
30216099 <martin.mosquera1@ucalgary.ca>
2025-03-15 11:13:15 -0600

    Added Final Functions? to gamesessionmanager

--- src/server/GameSessionManager.java
+++ src/server/GameSessionManager.java
@@ -54,7 +54,13 @@ public class GameSessionManager implements Runnable{
      * @return The player whose turn it is.
      */
     private Player getCurrentPlayer(){
-        return player1; 
+        int currentPlayerIndex = gameController.GetCurrentPlayer(); //Get the player's index
+        
+        //Retunr the corresponding player 
+        if (currentPlayerIndex == 0){
+            return player1;
+        }
+        return player2; 
     }
 
 
@@ -64,16 +70,33 @@ public class GameSessionManager implements Runnable{
      *  @return The received input message 
      */ 
     private ThreadMessage recieveInputFromPlayer(Player player){
-
+        try {
+            BlockingQueue<ThreadMessage> playerQueue = queue.get(player.getThread());
+            return playerQueue.take();  // Blocking call until input received from the queue
+        } catch (InterruptedException e){
+            Thread.currentThread().interrupt(); //Restore interrupted state
+            return null;
+        }
     }
 
     /**
      * Sends the current state of the game to the players 
      */
     private void GameStateToPlayers(){
+        ThreadMessage gameState = new ThreadMessage(Thread.currentThread(), formatGameState()); //Sender thread reference
 
+        sendToPlayer(player1, gameState); //Send state to players
+        sendToPlayer(player2, gameState);
     }
 
+    /**
+     * Formats the game state in a string format 
+     * @return The string format of current player, winner, and game status
+     */
+    private String formatGameState(){
+        return "Current Player: " + gameController.GetCurrentPlayer() + ", Winner: " + java.util.Arrays.toString(gameController.GetWinner()) + 
+                ", Game On Going: " + gameController.GetGameonGoing();
+    }
     /**
      *  Sends a message to a specific player
      * @param player the player send the message to 
@@ -81,20 +104,23 @@ public class GameSessionManager implements Runnable{
      */
     private void sendToPlayer(Player player, ThreadMessage message){
 
+        //Add message to player's queue
+        BlockingQueue<ThreadMessage> playerQueue = queue.get(player.getThread());
+        if (playerQueue !=null){
+            playerQueue.offer(message); 
+        }
     }
 
     /**
      * Ends the game if necessary and notifies both players
      */
     private void handleGameEnd(){
+        int[] winner = gameController.GetWinner(); //Get the winner of the game 
+
+        String resultMessage = (winner.length > 1) ? "Game ended in a tie" : "Player " + winner[0] + " won!";
+        sendToPlayer(player1, new ThreadMessage(Thread.currentThread(), resultMessage)); // Notify player 1 of the result
+        sendToPlayer(player2, new ThreadMessage(Thread.currentThread(), resultMessage)); // Notify player 2 of the result
 
     }
-
-
-
-
-
-
-
 
 }
