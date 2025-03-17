a8a536088a258324a1dbce9eac74ca89a42ba967
harry <153475377+illiteratewaffle@users.noreply.github.com>
2025-03-16 18:47:00 -0600

    copy fields and methods from uml to code

--- src/leaderboard/Leaderboard.java
+++ src/leaderboard/Leaderboard.java
@@ -1,4 +1,40 @@
 package leaderboard;
 
+import java.util.ArrayList;
+
 public class Leaderboard {
+    private ArrayList<String> rankings;
+
+    public Leaderboard() {
+
+    }
+
+    public ArrayList<String> getTopPlayers(){
+
+        return rankings;
+    }
+
+    public void updateLeaderboard(){
+
+    }
+
+    public ArrayList<String> sortPlayers() {
+
+        return rankings;
+    }
+
+    public ArrayList<String> toggleSortOrder(){
+
+        return rankings;
+    }
+
+    public ArrayList<String> searchPlayer(String username){
+
+        return rankings;
+    }
+
+    public ArrayList<String> filterFriends(ArrayList<String> friendList){
+
+        return rankings;
+    }
 }
