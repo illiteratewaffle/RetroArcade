fbbe41ee25185a5bae9e46d094177ad3c7cbef1c
30208300 <colby.campbell@ucalgary.ca>
2025-03-19 13:58:50 -0600

    Started working on a test for the ServerLogger class

---
+++ tests/server/management/ServerLoggerTest.java
@@ -0,0 +1,17 @@
+package server.management;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerLoggerTest {
+
+    @Test
+    void log() {
+        // We want to test that the log function prints
+    }
+
+    @Test
+    void close() {
+    }
+}
