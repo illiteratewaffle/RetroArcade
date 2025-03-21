25f79b06f196e351188dfdf08612f97bea6b219e
30208763 <evan.poulson@ucalgary.ca>
2025-03-21 15:29:22 -0600

    Started work on connection manager

---
+++ src/server/management/ConnectionManager.java
@@ -0,0 +1,39 @@
+package server.management;
+import server.player.PlayerHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ConnectionManager {
+    private final int port = 5050;
+    private final ServerController serverController;
+
+    public ConnectionManager(ServerController serverController) {
+        this.serverController = serverController;
+    }
+
+    public void startListening() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Connection Manager: Accepted connection from " + clientSocket.getRemoteSocketAddress());
+                //Create a new virtual thread for handling the new client connection.
+                Thread.startVirtualThread(() -> handleNewConnection(client));
+            }
+        } catch (IOException e) {
+            System.err.println("Connection Manager encountered an error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void handleNewConnection(Socket clientSocket) {
+        //Create a new player handler for the cient.
+        PlayerHandler handler = new PlayerHandler(clientSocket, serverController);
+        //Delegate the new PlayerHandler to the server controller for tracking and session management.
+        //serverController.registerPlayerHanlder(handler);
+        //Start the process of the handler listening for client messages.
+        //handler.listen();
+    }
+}
+
