fa57274075137036a54ebd1712f4add35b88bc00
30208763 <evan.poulson@ucalgary.ca>
2025-03-21 15:57:22 -0600

    Created ThreadRegistry

---
+++ src/server/management/ThreadRegistry.java
@@ -0,0 +1,8 @@
+package server.management;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ThreadRegistry {
+    //A centralized registry of all threads accessible by all network managers and server controller.
+    public static final ConcurrentHashMap<Thread, BlockingQueue<ThreadMessage>> threadRegistry = new ConcurrentHashMap<>();
+}
