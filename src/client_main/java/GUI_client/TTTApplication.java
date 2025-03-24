d1aa53d1aed0068289791438bbcd721683c5f64d
30210341 <tyler.jarrett@ucalgary.ca>
2025-03-24 14:48:22 -0600

    Game Menu UI

--- src/client_main/java/GUI_client/TTTApplication.java
+++ src/client_main/java/GUI_client/TTTApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class TTTApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TTTApplication.class.getResource("/GUI_client/TTT.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TTTApplication.class.getResource("/GUI_client/gameMenu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setResizable(false);
         stage.setTitle("Tic Tac Toe");
