512c53d97d10df4a7f1ce8b027afdded28e55090
30208763 <evan.poulson@ucalgary.ca>
2025-03-27 14:19:49 -0600

    Made changes to server controller and other classes lolzies

--- src/server/management/ThreadRegistry.java
+++ src/server/management/ThreadRegistry.java
@@ -3,7 +3,9 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ThreadRegistry {
-    // TODO: We should discuss whether we want to use a static class to contain the message queue for all threads instead of an instance of the class
+
     //A centralized registry of all threads accessible by all network managers and server controller.
-    public static final ConcurrentHashMap<Thread, BlockingQueue<ThreadMessage>> threadRegistry = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<Thread, BlockingQueue<ThreadMessage>> threadRegistry = new ConcurrentHashMap<>();
+
+
 }
