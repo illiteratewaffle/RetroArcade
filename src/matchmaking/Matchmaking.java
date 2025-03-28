57c86c4a97aa3da8b9a797af10240bccd2f9226f
30204884 <yash.dhaneshwari@ucalgary.ca>
2025-03-28 16:19:08 -0600

    Designig the functions needed will add actual code after theoretical designing is completed.

--- src/matchmaking/Matchmaking.java
+++ src/matchmaking/Matchmaking.java
@@ -1,5 +1,36 @@
 package matchmaking;
-
+import java.util.*;
 public class Matchmaking {
 
+    /***
+     * Match Opponents according to rank
+     */
+    public void matchOpponents(){
+    }
+
+    /***
+     * Matchmake friend invitations
+     */
+    public void matchFriends(){
+    }
+
+    /***
+     * Remove Player from queue upon cancellation or upon successfull matchmaking
+     */
+    public void dequeue(){
+    }
+
+    /***
+     * Enter players into queue
+     */
+    public void enque(){
+    }
+
+    /***
+     * Check if player is undergoing timeout penalty
+     * @return
+     */
+    public boolean checkTimeout(){
+        return true;
+    }
 }
--- src/matchmaking/Queue.java
+++ src/matchmaking/Queue.java
@@ -1,4 +1,11 @@
 package matchmaking;
-
+import java.util.*;
 public class Queue {
+
+    /***
+     * Sorts queue list according to specified rank, greatest to lowest
+     * @return Sorted ArrayList of Player's
+     */
+    public ArrayList<Player> sortQueue(){
+    }
 }
