dfb3d2ebdb3762e363d544bf77b1e14e6a498c70
10078303 <jeff.suen@ucalgary.ca>
2025-03-28 21:05:29 -0600

    module-info.java fixed

--- src/client_main/java/GameLogic_Client/Connect4/C4Main.java
+++ src/client_main/java/GameLogic_Client/Connect4/C4Main.java
@@ -9,7 +9,7 @@ public class C4Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/client_main/resources/GUI_client/connect4.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GUI_client/connect4.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         C4GUIController GUIcontroller = fxmlLoader.getController();
 
