847d3a570477050f7dbc5145c5c500864135daf1
10078303 <jeff.suen@ucalgary.ca>
2025-03-30 17:38:27 -0600

    edited fxml and gui controller to allow for compilation

--- src/client_main/java/GameLogic_Client/Connect4/C4GUIController.java
+++ src/client_main/java/GameLogic_Client/Connect4/C4GUIController.java
@@ -21,4 +21,40 @@ public class C4GUIController implements Initializable {
     private void handleUserClick() {
         gameLogic.ReceiveInput(new ivec2(3, 0)); // for example, drop in column 3
     }
+
+    @FXML
+    private void onCol0Click() {
+        gameLogic.ReceiveInput(new ivec2(0, 0));
+    }
+
+    @FXML
+    private void onCol1Click() {
+        gameLogic.ReceiveInput(new ivec2(1, 0));
+    }
+
+    @FXML
+    private void onCol2Click() {
+        gameLogic.ReceiveInput(new ivec2(2, 0));
+    }
+
+    @FXML
+    private void onCol3Click() {
+        gameLogic.ReceiveInput(new ivec2(3, 0));
+    }
+
+    @FXML
+    private void onCol4Click() {
+        gameLogic.ReceiveInput(new ivec2(4, 0));
+    }
+
+    @FXML
+    private void onCol5Click() {
+        gameLogic.ReceiveInput(new ivec2(5, 0));
+    }
+
+    @FXML
+    private void onCol6Click() {
+        gameLogic.ReceiveInput(new ivec2(6, 0));
+    }
+
 }
