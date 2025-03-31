e0eb5352e569a24fe8f128460eb2ec138ec6b614
30208763 <evan.poulson@ucalgary.ca>
2025-03-30 18:55:07 -0600

    Changed main to actually have stuff in it, and removed all the redundant stuff from server controller

---
+++ src/server/management/Main.java
@@ -0,0 +1,12 @@
+package server.management;
+
+public class Main {
+    /**
+     * Main Method to start the server
+     */
+    public static void main(String[] args) {
+        ServerController controller = new ServerController();
+        int port = 5050;
+        controller.startServer(port);
+    }
+}
