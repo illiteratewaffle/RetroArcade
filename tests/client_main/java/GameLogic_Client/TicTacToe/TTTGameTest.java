43fb969f58eed49d8c8b0a0f7f38653f95f3099f
30172781 <vicente.david@ucalgary.ca>
2025-03-30 23:08:14 -0600

    completed all tests and corrected all test failures particularly in the checkDraw(), and switchTurn() tests. Also cleaned up the code to make it more clean.

--- tests/client_main/java/GameLogic_Client/TicTacToe/TTTGameTest.java
+++ tests/client_main/java/GameLogic_Client/TicTacToe/TTTGameTest.java
@@ -1,4 +1,8 @@
 package client_main.java.GameLogic_Client.TicTacToe;
+/**
+ * TTGameTest class to test the functions and methods of the TTTGame class.
+ * Author: Vicente David - Game Logic Team
+ */
 
 import GameLogic_Client.TicTacToe.TTTBoard;
 import GameLogic_Client.TicTacToe.TTTGame;
@@ -8,76 +12,65 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 class TTTGameTest {
     private TTTGame game;
-    private TTTBoard board;
 
     @BeforeEach
     void setUp() {
         game = new TTTGame();
-        board = game.board;
     }
 
     @Test
     void makeMove() {
         assertTrue(game.makeMove(0, 0), "Move should be valid as the board is empty");
         assertFalse(game.makeMove(0, 0), "Move should be invalid as this tile is not empty");
-
-        assertFalse(game.makeMove(0, 3), "Move should be invalid as the board is empty");
     }
 
     @Test
     void switchTurn() {
-        int currentPlayer = game.currentPlayer;
-        assertEquals(1, currentPlayer, "Current player should be 1 at the start of the game");
+        assertEquals(1, game.currentPlayer, "Current player should be 1 at the start of the game");
         game.switchTurn();
-        assertEquals(2, currentPlayer, "Current player should be 2 after first turn ends");
+        assertEquals(2, game.currentPlayer, "Current player should be 2 after first turn ends");
         game.switchTurn();
-        assertEquals(1, currentPlayer, "Current player should be 1 after player 2 ends turn");
+        assertEquals(1, game.currentPlayer, "Current player should be 1 after player 2 ends turn");
     }
 
     @Test
     void checkWin() {
         game.makeMove(0, 0);
-        game.switchTurn();
         game.makeMove(1,0);
-        game.switchTurn();
         game.makeMove(0,1);
-        assertFalse(game.checkWin(board), "should return false as no player has achieved a win condition");
-        game.switchTurn();
+        assertFalse(game.checkWin(game.board), "should return false as no player has achieved a win condition");
         game.makeMove(1,1);
-        game.switchTurn();
         game.makeMove(0,2);
-        assertTrue(game.checkWin(board), "should return true as player 3 has a tile in the first row");
+        assertTrue(game.checkWin(game.board), "should return true as player 3 has a tile in the first row");
     }
 
     @Test
     void checkDraw() {
         game.makeMove(0, 0); //player 1
-        game.switchTurn();
-        game.makeMove(0,2);  //player 2
-        game.switchTurn();
-        game.makeMove(0,1);  //player 1
-        game.switchTurn();
-        game.makeMove(2,0);  //player 2
-        game.switchTurn();
-        game.makeMove(1,0);  //player 1
-        assertFalse(game.checkDraw(board), "Should return false as game is still ongoing and no win condition is met");
-        game.switchTurn();
+        game.makeMove(0,1);  //player 2
+        game.makeMove(0,2);  //player 1
         game.makeMove(1,1);  //player 2
-        game.switchTurn();
-        game.makeMove(1,2);  //player 1
-        game.switchTurn();
-        game.makeMove(2,1);  //player 2
-        game.switchTurn();
+        game.makeMove(1,0);  //player 1
+        assertFalse(game.checkDraw(game.board), "Should return false as game is still ongoing and no win condition is met");
+        game.makeMove(1,2);  //player 2
+        game.makeMove(2,1);  //player 1
+        game.makeMove(2,0);  //player 2
         game.makeMove(2,2);  //player 1
-        assertTrue(game.checkDraw(board), "should return true as no win condition is met and all tiles are full");
-
+        assertTrue(game.checkDraw(game.board), "should return true as no win condition is met and all tiles are full");
     }
 
     @Test
     void isGameOver() {
-    }
-
-    @Test
-    void printBoard() {
+        // NOTE: these tests are technically unnecessary and redundant as the isGameOver() function
+        // is already being tested by checkDraw() and checkWin() methods.
+        game.makeMove(0, 0); //player 1
+        game.makeMove(0,1); //player 2
+        game.makeMove(0,2); //player 1
+        assertFalse(game.isGameOver(game.board), "should return false as no player has achieved a win/draw condition");
+        game.makeMove(1,0); //player 2
+        game.makeMove(1,1); //player 1
+        game.makeMove(1,2); //player 2
+        game.makeMove(2,0); //player 1
+        assertTrue(game.isGameOver(game.board), "should return true as win condition is met by player 1");
     }
 }
