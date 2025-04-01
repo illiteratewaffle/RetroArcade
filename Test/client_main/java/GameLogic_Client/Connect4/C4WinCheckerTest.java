64bd31344ae4cfbea27983a14c7b7e7473a83290
Devanshi Iragavarapu <devanshi.iragavar1@ucalgary.ca>
2025-04-01 17:30:15 -0600

    Made test functions for forward slash win conditions and ensured the function works correctly.

--- Test/client_main/java/GameLogic_Client/Connect4/C4WinCheckerTest.java
+++ Test/client_main/java/GameLogic_Client/Connect4/C4WinCheckerTest.java
@@ -3,14 +3,13 @@ import GameLogic_Client.Connect4.C4WinChecker;
 import client_main.java.GameLogic_Client.Connect4.C4Piece;
 
 import org.junit.jupiter.api.*;
-import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class C4WinCheckerTest {
     private static final int ROWS = 6;
     private static final int COLS = 7;
-    private C4Piece[][] board;
+    private client_main.java.GameLogic_Client.Connect4.C4Piece[][] board;
 
     @BeforeEach
     void setUp() {
@@ -31,7 +30,7 @@ class C4WinCheckerTest {
         board[winRow][2] = C4Piece.BLUE;
         board[winRow][3] = C4Piece.BLUE;
 
-        assertTrue(C4WinChecker.checkC4Horizontal(winRow, 1, C4Piece.BLUE, board));
+        assertTrue(C4WinChecker.checkC4Horizontal(winRow, C4Piece.BLUE, board));
     }
 
 
@@ -43,7 +42,7 @@ class C4WinCheckerTest {
         board[winRow][5] = C4Piece.BLUE;
         board[winRow][6] = C4Piece.BLUE;
 
-        assertTrue(C4WinChecker.checkC4Horizontal(winRow, 3, C4Piece.BLUE, board));
+        assertTrue(C4WinChecker.checkC4Horizontal(winRow, C4Piece.BLUE, board));
     }
 
 
@@ -58,7 +57,7 @@ class C4WinCheckerTest {
         board[winRow][5] = C4Piece.BLUE;
         board[winRow][6] = C4Piece.BLUE;
 
-        assertTrue(C4WinChecker.checkC4Horizontal(winRow, 2, C4Piece.BLUE, board));
+        assertTrue(C4WinChecker.checkC4Horizontal(winRow, C4Piece.BLUE, board));
     }
 
 
@@ -73,7 +72,7 @@ class C4WinCheckerTest {
         board[winRow][5] = C4Piece.BLUE;
         board[winRow][6] = C4Piece.BLUE;
 
-        assertTrue(C4WinChecker.checkC4Horizontal(winRow, 0, C4Piece.BLUE, board));
+        assertTrue(C4WinChecker.checkC4Horizontal(winRow, C4Piece.BLUE, board));
     }
 
 
@@ -85,7 +84,7 @@ class C4WinCheckerTest {
         board[winRow][5] = C4Piece.BLUE;
         board[winRow][6] = C4Piece.BLUE;
 
-        assertTrue(C4WinChecker.checkC4Horizontal(winRow, 6, C4Piece.BLUE, board));
+        assertTrue(C4WinChecker.checkC4Horizontal(winRow, C4Piece.BLUE, board));
     }
 
 
@@ -100,7 +99,7 @@ class C4WinCheckerTest {
         board[winRow][5] = C4Piece.BLUE;
         board[winRow][6] = C4Piece.BLUE;
 
-        assertTrue(C4WinChecker.checkC4Horizontal(winRow, 4, C4Piece.BLUE, board));
+        assertTrue(C4WinChecker.checkC4Horizontal(winRow, C4Piece.BLUE, board));
     }
 
 
@@ -113,7 +112,7 @@ class C4WinCheckerTest {
         board[winRow][5] = C4Piece.BLUE;
         board[winRow][6] = C4Piece.BLUE;
 
-        assertFalse(C4WinChecker.checkC4Horizontal(winRow, 3, C4Piece.BLUE, board));
+        assertFalse(C4WinChecker.checkC4Horizontal(winRow, C4Piece.BLUE, board));
     }
 
 
@@ -124,7 +123,7 @@ class C4WinCheckerTest {
         board[winRow][5] = C4Piece.BLUE;
         board[winRow][6] = C4Piece.BLUE;
 
-        assertFalse(C4WinChecker.checkC4Horizontal(winRow, 5, C4Piece.BLUE, board));
+        assertFalse(C4WinChecker.checkC4Horizontal(winRow, C4Piece.BLUE, board));
     }
 
 
@@ -135,7 +134,7 @@ class C4WinCheckerTest {
         board[winRow][5] = C4Piece.BLUE;
         board[winRow][6] = C4Piece.BLUE;
 
-        assertFalse(C4WinChecker.checkC4Horizontal(winRow, 3, C4Piece.BLUE, board));
+        assertFalse(C4WinChecker.checkC4Horizontal(winRow, C4Piece.BLUE, board));
     }
 
 
@@ -147,8 +146,184 @@ class C4WinCheckerTest {
         board[winRow][5] = C4Piece.BLUE;
         board[winRow][6] = C4Piece.BLUE;
 
-        assertFalse(C4WinChecker.checkC4Horizontal(winRow, 3, C4Piece.BLUE, board));
+        assertFalse(C4WinChecker.checkC4Horizontal(winRow, C4Piece.BLUE, board));
     }
 
 
+    /**
+     * TEST CASES FOR VERTICAL WIN CONDITION
+     */
+    @Test
+    void verticalWin1() throws Exception {
+        int winCol = 2;
+        board[0][winCol] = C4Piece.RED;
+        board[1][winCol] = C4Piece.RED;
+        board[2][winCol] = C4Piece.RED;
+        board[3][winCol] = C4Piece.RED;
+
+        assertTrue(C4WinChecker.checkC4Vertical(0, winCol, C4Piece.RED, board));
+    }
+
+    @Test
+    void verticalWin2() throws Exception {
+        int winCol = 3;
+        board[2][winCol] = C4Piece.RED;
+        board[3][winCol] = C4Piece.RED;
+        board[4][winCol] = C4Piece.RED;
+        board[5][winCol] = C4Piece.RED;
+
+        assertTrue(C4WinChecker.checkC4Vertical(2, winCol, C4Piece.RED, board));
+    }
+
+    @Test
+    void verticalWin3() throws Exception {
+        int winCol = 5;
+        board[0][winCol] = C4Piece.BLUE;
+        board[1][winCol] = C4Piece.BLUE;
+        board[2][winCol] = C4Piece.BLUE;
+        board[3][winCol] = C4Piece.BLUE;
+        board[4][winCol] = C4Piece.BLUE; // More than 4 in a row
+
+        assertTrue(C4WinChecker.checkC4Vertical(0, winCol, C4Piece.BLUE, board));
+    }
+
+    @Test
+    void verticalNoWin1() throws Exception {
+        int winCol = 6;
+        board[0][winCol] = C4Piece.BLANK;
+        board[1][winCol] = C4Piece.RED;
+        board[2][winCol] = C4Piece.BLUE;
+        board[4][winCol] = C4Piece.BLUE;
+        board[5][winCol] = C4Piece.BLUE;
+        assertFalse(C4WinChecker.checkC4Vertical(2, winCol, C4Piece.RED, board));
+    }
+
+    @Test
+    void verticalNoWin2() throws Exception {
+        int winCol = 4;
+        board[1][winCol] = C4Piece.BLUE;
+        board[2][winCol] = C4Piece.BLUE;
+        board[4][winCol] = C4Piece.BLUE;
+        board[5][winCol] = C4Piece.BLUE;
+
+        assertFalse(C4WinChecker.checkC4Vertical(1, winCol, C4Piece.BLUE, board));
+    }
+
+    @Test
+    void verticalNoWin3() throws Exception {
+        int winCol = 1;
+        board[0][winCol] = C4Piece.RED;
+        board[1][winCol] = C4Piece.RED;
+        board[2][winCol] = C4Piece.RED;
+
+        assertFalse(C4WinChecker.checkC4Vertical(0, winCol, C4Piece.RED, board));
+    }
+
+    @Test
+    void verticalNoWin4() throws Exception {
+        int winCol = 0;
+        assertFalse(C4WinChecker.checkC4Vertical(0, winCol, C4Piece.RED, board));
+    }
+
+    @Test
+    void verticalNoWin5() throws Exception {
+        int winCol = 6;
+        board[1][winCol] = C4Piece.BLUE;
+        board[2][winCol] = C4Piece.RED;
+        board[4][winCol] = C4Piece.BLUE;
+        board[5][winCol] = C4Piece.BLUE;
+        assertFalse(C4WinChecker.checkC4Vertical(2, winCol, C4Piece.RED, board));
+    }
+
+
+    /***
+     * TEST CASES FOR FORWARD SLASH WIN
+     */
+    @Test
+    void forwardSlashWin1() throws Exception {
+        board[3][1] = C4Piece.RED;
+        board[2][2] = C4Piece.RED;
+        board[1][3] = C4Piece.RED;
+        board[0][4] = C4Piece.RED;
+
+        assertTrue(C4WinChecker.checkC4ForwardSlash(3, 1, C4Piece.RED, board));
+    }
+
+    @Test
+    void forwardSlashWin2() throws Exception {
+        board[4][2] = C4Piece.RED;
+        board[3][3] = C4Piece.RED;
+        board[2][4] = C4Piece.RED;
+        board[1][5] = C4Piece.RED;
+
+        assertTrue(C4WinChecker.checkC4ForwardSlash(3, 3, C4Piece.RED, board));
+    }
+
+    @Test
+    void forwardSlashWin3() throws Exception {
+        board[5][0] = C4Piece.BLUE;
+        board[4][1] = C4Piece.BLUE;
+        board[3][2] = C4Piece.BLUE;
+        board[2][3] = C4Piece.BLUE;
+        board[1][4] = C4Piece.BLUE;
+        board[0][5] = C4Piece.BLUE;
+
+        assertTrue(C4WinChecker.checkC4ForwardSlash(3, 2, C4Piece.BLUE, board));
+    }
+
+    @Test
+    void forwardSlashWin4() throws Exception {
+        board[3][3] = C4Piece.BLUE;
+        board[2][4] = C4Piece.BLUE;
+        board[1][5] = C4Piece.BLUE;
+        board[0][6] = C4Piece.BLUE; // Top-right corner
+
+        assertTrue(C4WinChecker.checkC4ForwardSlash(2, 4, C4Piece.BLUE, board));
+    }
+
+    @Test
+    void forwardSlashWin5() throws Exception {
+        board[5][0] = C4Piece.RED;
+        board[4][1] = C4Piece.RED;
+        board[3][2] = C4Piece.RED;
+        board[2][3] = C4Piece.RED; // Bottom-left corner
+
+        assertTrue(C4WinChecker.checkC4ForwardSlash(5, 0, C4Piece.RED, board));
+    }
+
+    @Test
+    void forwardSlashNoWin1() throws Exception {
+        board[5][1] = C4Piece.RED;
+        board[4][2] = C4Piece.RED;
+        board[2][4] = C4Piece.RED;
+        board[1][5] = C4Piece.RED;
+
+        assertFalse(C4WinChecker.checkC4ForwardSlash(5, 1, C4Piece.RED, board));
+    }
+
+    @Test
+    void forwardSlashNoWin2() throws Exception {
+        board[5][1] = C4Piece.RED;
+        board[4][2] = C4Piece.RED;
+        board[3][3] = C4Piece.RED; // Only three in a row
+
+        assertFalse(C4WinChecker.checkC4ForwardSlash(5, 1, C4Piece.RED, board));
+    }
+
+    @Test
+    void forwardSlashNoWin3() throws Exception {
+        assertFalse(C4WinChecker.checkC4ForwardSlash(2, 2, C4Piece.RED, board));
+    }
+
+
+    @Test
+    void forwardSlashNoWin4() throws Exception {
+        board[5][0] = C4Piece.RED;
+        board[4][1] = C4Piece.BLUE; // Different piece here breaks the streak
+        board[3][2] = C4Piece.RED;
+        board[2][3] = C4Piece.RED;
+        board[1][4] = C4Piece.RED;
+
+        assertFalse(C4WinChecker.checkC4ForwardSlash(5, 0, C4Piece.RED, board));
+    }
 }
--- src/client_main/java/GameLogic_Client/Connect4/C4WinChecker.java
+++ src/client_main/java/GameLogic_Client/Connect4/C4WinChecker.java
@@ -29,7 +29,7 @@ public class C4WinChecker {
      * @return true if a win condition is met, false otherwise.
      */
     private static boolean checkC4WinCondition(int row, int col, C4Piece piece, C4Piece[][] c4Board) {
-        return checkC4Horizontal(row, col, piece, c4Board) || checkC4Vertical(row, col, piece, c4Board) ||
+        return checkC4Horizontal(row, piece, c4Board) || checkC4Vertical(row, col, piece, c4Board) ||
                 checkC4ForwardSlash(row, col, piece, c4Board) || checkC4BackwardSlash(row, col, piece, c4Board);
     }
 
@@ -37,12 +37,11 @@ public class C4WinChecker {
      * Checks for a horizontal win condition.
      *
      * @param row     the row to check.
-     * @param col     the column to check.
      * @param piece   the piece placed.
      * @param c4Board the 2-d representation of the board.
      * @return true if a horizontal win is detected, false otherwise.
      */
-    public static boolean checkC4Horizontal(int row, int col, C4Piece piece, C4Piece[][] c4Board) {
+    public static boolean checkC4Horizontal(int row, C4Piece piece, C4Piece[][] c4Board) {
         int pieceCounter = 0;
 
         // Iterate through the entire row
@@ -69,7 +68,7 @@ public class C4WinChecker {
      * @param c4Board the 2-d representation of the board.
      * @return true if a vertical win is detected, false otherwise.
      */
-    private static boolean checkC4Vertical(int row, int col, C4Piece piece, C4Piece[][] c4Board) {
+    public static boolean checkC4Vertical(int row, int col, C4Piece piece, C4Piece[][] c4Board) {
         int pieceCounter = 1;
 
         // Go down of piece placed till the end of the board.
@@ -77,11 +76,11 @@ public class C4WinChecker {
         for(int i = row+1; i < c4Board.length; i++){
             if(c4Board[i][col] == piece){
                 pieceCounter += 1;              // Increment piece counter if piece found.
+                if(pieceCounter >= 4) {
+                    return true;                    // Returns true if there is 4 in a row consecutively.
+                }
             } else {
-                break;                          // Else reset it to just 0, so it only takes consecutive pieces into account for win.
-            }
-            if(pieceCounter >= 4) {
-                return true;                    // Returns true if there is 4 in a row consecutively.
+                pieceCounter = 0;                 // Else reset it to just 0, so it only takes consecutive pieces into account for win.
             }
         }
 
@@ -97,7 +96,7 @@ public class C4WinChecker {
      * @param c4Board the 2-d representation of the board.
      * @return true if a forward slash diagonal win is detected, false otherwise.
      */
-    private static boolean checkC4ForwardSlash(int row, int col, C4Piece piece, C4Piece[][] c4Board) {
+    public static boolean checkC4ForwardSlash(int row, int col, C4Piece piece, C4Piece[][] c4Board) {
         int pieceCounter = 1;
 
         // Loop to check coordinates to the piece's top-right.
