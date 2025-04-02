b5fdbc8d21b0f9fcf46223bd9826ade66f4a2a49
10078303 <jeff.suen@ucalgary.ca>
2025-04-01 22:56:23 -0600

    Added the module-info.java in the correct folder

---
+++ src/client_main/java/module-info.java
@@ -0,0 +1,11 @@
+module seng300.w25.project {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.media; // if using media like sounds or video
+    requires java.sql;     // optional
+
+    exports GUI_client;
+    exports GameLogic_Client;
+
+    opens GUI_client to javafx.fxml, javafx.graphics;
+}
