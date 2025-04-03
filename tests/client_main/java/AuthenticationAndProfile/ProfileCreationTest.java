e0d8e6f881542ffe0068d9d632c7d3869abb79e2
“30119655” <ahsan.tariq@ucalgary.ca>
2025-04-03 17:00:28 -0600

    Added tests for blank hashed passwords and simple hashed password. Refactored code (moved resources folder to root and renamed old resources folder to assets)

--- tests/client_main/java/AuthenticationAndProfile/ProfileCreationTest.java
+++ tests/client_main/java/AuthenticationAndProfile/ProfileCreationTest.java
@@ -7,8 +7,19 @@ import server.player.PlayerManager;
 class ProfileCreationTest {
 
     @Test
-    void hashedPassword() {
+    void validHashedPassword() {
+        String password = "1234567";
+        String hashedPassword = ProfileCreation.hashedPassword(password);
+        assertEquals("8BB0CF6EB9B17D0F7D22B456F121257DC1254E1F01665370476383EA776DF414", hashedPassword);
     }
+
+    @Test
+    void blankHashedPassword() {
+        String password = "";
+        String hashedPassword = ProfileCreation.hashedPassword(password);
+        assertEquals("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855".toUpperCase(), hashedPassword);
+    }
+
 
     @Test
     void createNewProfile() {
