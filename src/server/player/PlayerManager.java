337fde62c9ad4dd5f91426f6bfc72ea080084acb
10137257 <cristian.otalora1@ucalgary.ca>
2025-04-03 12:02:43 -0600

    Created getAttribute method for fetching attribute entry for specified player id.

--- src/server/player/PlayerManager.java
+++ src/server/player/PlayerManager.java
@@ -251,6 +251,23 @@ public class PlayerManager {
         }
     }
 
+    public static String getAttribute(int id, String attrColumn) {
+        String query = "SELECT " + attrColumn + " FROM profiles WHERE id = ?";
+        try (PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                return rs.getString(attrColumn);
+            } else {
+                System.err.println("No player found with ID: " + id);
+                return null;
+            }
+        } catch (SQLException e) {
+            System.err.println("Error retrieving attribute '" + attrColumn + "' for player ID " + id + ": " + e.getMessage());
+            return null;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(updateAttribute(1, "email", "newEmail@me.ca" ));
     }
