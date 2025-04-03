c429f88fa1634313961a006db40d18162b7f371e
“30119655” <ahsan.tariq@ucalgary.ca>
2025-04-03 16:33:06 -0600

    Started ProfileCreationTest, not working

--- tests/client_main/java/AuthenticationAndProfile/ProfileCreationTest.java
+++ tests/client_main/java/AuthenticationAndProfile/ProfileCreationTest.java
@@ -2,6 +2,7 @@ package client_main.java.AuthenticationAndProfile;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import server.player.PlayerManager;
 
 class ProfileCreationTest {
 
@@ -11,5 +12,7 @@ class ProfileCreationTest {
 
     @Test
     void createNewProfile() {
+        ProfileCreation.createNewProfile("ProfileCreationTest", "ProfileCreationTest@email.com", "ProfileCreationTestPassword");
+        assertEquals("ProfileCreationTest", PlayerManager.getAttribute(10, "username"));
     }
 }
