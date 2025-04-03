31511bfc8f272eb044d81b7b2484cd21454fe4dc
10137257 <cristian.otalora1@ucalgary.ca>
2025-04-03 10:36:46 -0600

    Created new networking branch based on Mainlt2.

---
+++ src/server/Main.java
@@ -0,0 +1,24 @@
+package server;
+
+import server.management.ServerController;
+
+import java.util.Scanner;
+
+public class Main {
+    /**
+     * Main Method to start the server
+     */
+    public static void main(String[] args) {
+        ServerController controller = new ServerController();
+        int port = 5050;
+        controller.startServer(port);
+        // Create an infinite while loop so the server does not exit
+        // TODO: the main thread should be used for a proper task
+        Scanner sc = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            if (sc.nextLine().equalsIgnoreCase("exit"))
+                running = false;
+        }
+    }
+}
