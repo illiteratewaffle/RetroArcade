939fdba459708cbbd355bb057c2964d85e315082
30020534 <sylest.holzwarth@ucalgary.ca>
2025-04-03 18:10:11 -0600

    more work on the leaderboard.fxml

--- src/client_main/java/GUI_client/leaderboardController.java
+++ src/client_main/java/GUI_client/leaderboardController.java
@@ -1,12 +1,34 @@
 package GUI_client;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-    public class leaderboardController {
-        }
+public class leaderboardController {
+    //Note that I gave all attributes a fx:id name and on action names. You can change these if you want.
+    @FXML
+    public ImageView checkers_button; //action: checkers_stats
+    @FXML
+    public ImageView TTT_button; //action: TTT_stats
+    @FXML
+    public ImageView C4_button; //action: C4_stats
+    @FXML
+    public ImageView search_button; //action: send_search
+    @FXML
+    public SplitMenuButton filter_stats; //action: filter
+    @FXML
+    public TextField friend_search; //action: search
+
+    //I am not sure if we will need 3 separate tables, or if we can simply refresh the table content based on the game button clicked.
+    @FXML
+    public TableView C4_table; //no action created.
+    @FXML
+    public TableView TTT_table; //no action created.
+    @FXML
+    public TableView checkers_table; //no action created.
+}
+
 
 
