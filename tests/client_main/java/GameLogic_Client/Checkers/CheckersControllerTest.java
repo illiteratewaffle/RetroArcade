146307ae01693b2afab0f541d58ea3d2c3a44d88
30213188 <ashton.giles@ucalgary.ca>
2025-04-05 13:00:38 -0600

    set up imports for classes to test and imports for junit libraries

--- tests/client_main/java/GameLogic_Client/Checkers/CheckersControllerTest.java
+++ tests/client_main/java/GameLogic_Client/Checkers/CheckersControllerTest.java
@@ -1,4 +1,10 @@
 package client_main.java.GameLogic_Client.Checkers;
 
+import GameLogic_Client.Checkers.CheckersController;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CheckersControllerTest {
+
 }
--- tests/client_main/java/GameLogic_Client/Checkers/CheckersHighlightBoardTest.java
+++ tests/client_main/java/GameLogic_Client/Checkers/CheckersHighlightBoardTest.java
@@ -1,4 +1,10 @@
 package client_main.java.GameLogic_Client.Checkers;
 
+import GameLogic_Client.Checkers.CheckersHighlightBoard;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CheckersHighlightBoardTest {
+
 }
