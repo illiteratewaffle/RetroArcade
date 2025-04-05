f562b7ab98e841ab4104b16381474fb6ef04fd6b
30210341 <tyler.jarrett@ucalgary.ca>
2025-04-05 12:59:14 -0600

    Changes mute condition for quitting

--- src/client_main/java/GUI_client/MainApplication.java
+++ src/client_main/java/GUI_client/MainApplication.java
@@ -8,10 +8,10 @@ import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-public class TTTApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TTTApplication.class.getResource("gameMenu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("gameMenu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setResizable(false);
         stage.initStyle(StageStyle.TRANSPARENT);
