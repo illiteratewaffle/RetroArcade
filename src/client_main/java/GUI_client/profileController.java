3c9a8ed936dd2c0cce118debf0820a6438788901
30020534 <sylest.holzwarth@ucalgary.ca>
2025-04-05 21:43:07 -0600

    Minor changes to profile.fxml and starting on profileController.

--- src/client_main/java/GUI_client/profileController.java
+++ src/client_main/java/GUI_client/profileController.java
@@ -1,4 +1,24 @@
 package GUI_client;
 
+import javafx.scene.input.MouseEvent;
+
 public class profileController {
+    public void go_home(MouseEvent mouseEvent) {
+
+    }
+
+    public void close_window(MouseEvent mouseEvent) {
+    }
+
+    public void open_inbox(MouseEvent mouseEvent) {
+    }
+
+    public void open_history(MouseEvent mouseEvent) {
+    }
+
+    public void open_stats(MouseEvent mouseEvent) {
+    }
+
+    public void open_edit_profile(MouseEvent mouseEvent) {
+    }
 }
