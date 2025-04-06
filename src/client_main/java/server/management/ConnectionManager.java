6a087d36de360438205b544b329bcb803cea67b8
30208763 <evan.poulson@ucalgary.ca>
2025-04-05 19:37:50 -0600

    Updated ConnectionManager

--- src/client_main/java/server/management/ConnectionManager.java
+++ src/client_main/java/server/management/ConnectionManager.java
@@ -1,10 +1,10 @@
 package server.management;
+import server.player.PlayerHandler;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import static server.management.ServerLogger.log;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class ConnectionManager implements Runnable {
     private final int port;
@@ -20,22 +20,28 @@ public class ConnectionManager implements Runnable {
     public void run() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
 
-            log("Connection Manager: Listening on port " + port);
+            System.out.println("Connection Manager: Listening on port " + port);
             //Start a loop that constantly listens for clients to accept.
             while (true) {
 
                 Socket clientSocket = serverSocket.accept();
-                log("Connection Manager: Accepted connection from " + clientSocket.getRemoteSocketAddress());
+                System.out.println("Connection Manager: Accepted connection from " + clientSocket.getRemoteSocketAddress());
 
-                // Authenticate the player before accepting them to the server
-                AuthenticateClient authenticateClient = new AuthenticateClient(clientSocket);
-                Thread.ofVirtual().start(authenticateClient);
+                //Create a blocking queue and player handler to handle the player connection on the server side.
+                LinkedBlockingQueue queue = new LinkedBlockingQueue();
+                PlayerHandler playerHandler = new PlayerHandler(clientSocket, queue);
+
+                //Create a thread for the player and start it.
+                Thread playerThread = Thread.ofVirtual().start(playerHandler);
 
                 //Store the players handlers thread and blocking queue on the thread registry.
+                ThreadRegistry.register(playerThread, queue);
             }
         } catch (IOException e) {
+
             //Throw a message to the console and print a stack trace if there is an error.
-            log("Connection Manager encountered an error: " + e.getMessage());
+            System.err.println("Connection Manager encountered an error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
