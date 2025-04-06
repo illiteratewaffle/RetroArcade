27f4a6e3d901bdb8cac5f0f9f7fd9c4850c88bc6
30213188 <ashton.giles@ucalgary.ca>
2025-04-06 11:41:24 -0600

    setup controller tests

--- tests/client_main/java/GameLogic_Client/Checkers/CheckersControllerTest.java
+++ tests/client_main/java/GameLogic_Client/Checkers/CheckersControllerTest.java
@@ -4,7 +4,19 @@ import GameLogic_Client.Checkers.CheckersController;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 public class CheckersControllerTest {
+    private CheckersController checkersController;
+
+    @BeforeEach
+    public void setUp() {
+        checkersController = new CheckersController();
+    }
+
+    @Test
+    public void testCheckersController() {
+
+    }
 
 }
