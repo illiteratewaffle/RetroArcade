74c1db0aa18d8d65e243668e77461e2e89a2989d
“30119655” <ahsan.tariq@ucalgary.ca>
2025-04-06 11:19:43 -0600

    Created GameHistoryTest.java and created getGameHistory test case

---
+++ tests/client_main_tests/java/AuthenticationAndProfile/GameHistoryTest.java
@@ -0,0 +1,23 @@
+package client_main_tests.java.AuthenticationAndProfile;
+
+import AuthenticationAndProfile.GameHistory;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameHistoryTest {
+    @Test
+    void getGameHistory() {
+        HashMap<String, Double> achievementProgress = new HashMap<>();
+        achievementProgress.put("Win_Streak", 0.75);
+        achievementProgress.put("Matches_Played", 0.50);
+        List<String> gameHistory = new ArrayList<>(List.of("TTT", "Checkers", "Connect4", "Checkers"));
+        GameHistory gh = new GameHistory(gameHistory, achievementProgress);
+        assertEquals(gameHistory, gh.getGameHistory());
+        assertEquals(achievementProgress, gh.getAchievementProgress());
+    }
+}
