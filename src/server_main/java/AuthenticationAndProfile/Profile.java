0900804cc74d56ef213cf8d18a08b5f060f3eb4c
“30119655” <ahsan.tariq@ucalgary.ca>
2025-04-06 15:53:36 -0600

    Fixed merging errors in Profile.java

--- src/server_main/java/AuthenticationAndProfile/Profile.java
+++ src/server_main/java/AuthenticationAndProfile/Profile.java
@@ -47,6 +47,7 @@ public class Profile {
 
     /**
      * Gets a player's email.
+     *
      * @return the current email of the player.
      */
     public String getEmail() {
@@ -55,19 +56,21 @@ public class Profile {
 
     /**
      * Sets a player's email.
+     *
      * @param newEmail the new email of the player.
      */
-    public void setEmail(String newEmail) throws SQLException{
+    public void setEmail(String newEmail) throws SQLException {
         this.email = newEmail;
         try {
             PlayerManager.updateAttribute(id, "email", newEmail);
-        } catch (SQLException s){
+        } catch (SQLException s) {
             throw new SQLException(s.getMessage());
         }
     }
 
     /**
      * Gets the hashed password of a player's account.
+     *
      * @return the player's hashed password.
      */
     public String getHashedPassword() {
@@ -76,9 +79,10 @@ public class Profile {
 
     /**
      * Sets a player's hashed password. Takes the inputted password, hashed it, then saves to profile.
+     *
      * @param newPassword the new password of the player.
      */
-    public void setHashedPassword(String newPassword) throws SQLException{
+    public void setHashedPassword(String newPassword) throws SQLException {
         String newHashedPassword = ProfileCreation.hashedPassword(newPassword);
         this.hashedPassword = newHashedPassword;
         try {
@@ -96,6 +100,7 @@ public class Profile {
 
     /**
      * Gets the player's nickname.
+     *
      * @return the player's nickname.
      */
     public String getNickname() {
@@ -104,9 +109,10 @@ public class Profile {
 
     /**
      * Sets a player's nickname.
+     *
      * @param newNickname the new nickname of the player.
      */
-    public void setNickname(String newNickname) throws SQLException{
+    public void setNickname(String newNickname) throws SQLException {
         this.nickname = newNickname;
         try {
             PlayerManager.updateAttribute(id, "nickname", newNickname);
@@ -122,6 +128,7 @@ public class Profile {
 
     /**
      * Gets the player's bio.
+     *
      * @return the player's bio.
      */
     public String getBio() {
@@ -130,13 +137,14 @@ public class Profile {
 
     /**
      * Sets a player's bio.
+     *
      * @param newBio the new bio of the player.
      */
-    public void setBio(String newBio) throws SQLException{
+    public void setBio(String newBio) throws SQLException {
         this.bio = newBio;
         try {
             PlayerManager.updateAttribute(id, "bio", newBio);
-        }catch (SQLException s) {
+        } catch (SQLException s) {
             throw new SQLException(s.getMessage());
         }
         try {
@@ -148,117 +156,116 @@ public class Profile {
 
     /**
      * Sets a player's online status.
+     *
      * @param online the new online status of the player.
      */
     public void setOnlineStatus(boolean online) throws SQLException {
-        public void setOnlineStatus ( boolean online) throws SQLException {
-            isOnline = online;
-            try {
-                if (online) {
-                    PlayerManager.updateAttribute(this.id, Integer.toString(ProfileCSVReader.ONLINE_INDEX), "true");
-                } else {
-                    PlayerManager.updateAttribute(this.id, Integer.toString(ProfileCSVReader.ONLINE_INDEX), "false");
-                }
-            } catch (SQLException s) {
-                throw new SQLException(s.getMessage());
-            }
-        }
-
-        public boolean getOnlineStatus () {
-            return isOnline;
-        }
-
-        /**
-         * Check if a player is in a game. If so, prints out the game they're playing (likely usable by GUI team).
-         * If not, prints out "Online" if the player is online. Otherwise, prints out "Offline".
-         */
-        public void getCurrentStatus () {
-            if (isOnline) {
-                if (currentGame != null) {
-                    System.out.println("Online - Currently playing: " + currentGame);
-                } else {
-                    System.out.println("Online");
-                }
+        isOnline = online;
+        try {
+            if (online) {
+                PlayerManager.updateAttribute(this.id, Integer.toString(ProfileCSVReader.ONLINE_INDEX), "true");
             } else {
-                System.out.println("Offline");
+                PlayerManager.updateAttribute(this.id, Integer.toString(ProfileCSVReader.ONLINE_INDEX), "false");
             }
+        } catch (SQLException s) {
+            throw new SQLException(s.getMessage());
         }
+    }
 
-        /**
-         * Gets the player's current game.
-         * @return the player's current game.
-         */
-        public String getCurrentGame () {
-            return currentGame;
-        }
+    public boolean getOnlineStatus() {
+        return isOnline;
+    }
 
-        /**
-         * Sets a player's current game.
-         * @param currentGame the current game of the player.
-         */
-        public void setCurrentGame (String currentGame) throws SQLException {
-            this.currentGame = currentGame;
-            try {
-                PlayerManager.updateAttribute(id, "current_game", currentGame);
-            } catch (SQLException s) {
-                throw new SQLException(s.getMessage());
+    /**
+     * Check if a player is in a game. If so, prints out the game they're playing (likely usable by GUI team).
+     * If not, prints out "Online" if the player is online. Otherwise, prints out "Offline".
+     */
+    public void getCurrentStatus() {
+        if (isOnline) {
+            if (currentGame != null) {
+                System.out.println("Online - Currently playing: " + currentGame);
+            } else {
+                System.out.println("Online");
             }
-            try {
-                PlayerManager.updateAttribute(id, "current_game", currentGame);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+        } else {
+            System.out.println("Offline");
         }
+    }
 
-        /**
-         * Gets the player's played games.
-         * @return the player's played games.
-         */
-        public GameHistory getGameHistory () {
-            return gameHistory;
+    /**
+     * Gets the player's current game.
+     *
+     * @return the player's current game.
+     */
+    public String getCurrentGame() {
+        return currentGame;
+    }
+
+    /**
+     * Sets a player's current game.
+     *
+     * @param currentGame the current game of the player.
+     */
+    public void setCurrentGame(String currentGame) throws SQLException {
+        this.currentGame = currentGame;
+        try {
+            PlayerManager.updateAttribute(id, "current_game", currentGame);
+        } catch (SQLException s) {
+            throw new SQLException(s.getMessage());
         }
-
-        /**
-         * Sets the player's played games.
-         * @param gameHistory the new played games of the player.
-         */
-        public void setGameHistory (GameHistory gameHistory){
-            this.gameHistory = gameHistory;
+        try {
+            PlayerManager.updateAttribute(id, "current_game", currentGame);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+    }
 
-        /**
-         * Gets the player's profile picture.
-         * @return the player's profile picture.
-         */
-        public String getProfilePicFilePath () {
-            return profilePicFilePath;
-        }
+    /**
+     * Gets the player's played games.
+     *
+     * @return the player's played games.
+     */
+    public GameHistory getGameHistory() {
+        return gameHistory;
+    }
 
-        /**
-         * Sets the player's profile picture.
-         * @param profilePicFilePath the new profile picture of the player.
-         */
-        public void setProfilePicFilePath (String profilePicFilePath){
-            this.profilePicFilePath = profilePicFilePath;
-        }
+    /**
+     * Sets the player's played games.
+     *
+     * @param gameHistory the new played games of the player.
+     */
+    public void setGameHistory(GameHistory gameHistory) {
+        this.gameHistory = gameHistory;
+    }
 
-        /**
-         * Gets the player's username.
-         * @return the player's username.
-         */
-        public static String exportUsername ( int id) throws SQLException {
-            try {
-                return PlayerManager.getUsername(id);
-            } catch (SQLException s) {
-                throw new SQLException(s.getMessage());
-            }
-            public static String exportUsername ( int id){
-                try {
-                    return PlayerManager.getUsername(id);
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
+    /**
+     * Gets the player's profile picture.
+     *
+     * @return the player's profile picture.
+     */
+    public String getProfilePicFilePath() {
+        return profilePicFilePath;
+    }
+
+    /**
+     * Sets the player's profile picture.
+     *
+     * @param profilePicFilePath the new profile picture of the player.
+     */
+    public void setProfilePicFilePath(String profilePicFilePath) {
+        this.profilePicFilePath = profilePicFilePath;
+    }
+
+    /**
+     * Gets the player's username.
+     *
+     * @return the player's username.
+     */
+    public static String exportUsername(int id) throws SQLException {
+        try {
+            return PlayerManager.getUsername(id);
+        } catch (SQLException s) {
+            throw new SQLException(s.getMessage());
         }
     }
 
@@ -266,11 +273,13 @@ public class Profile {
         return username;
     }
 
+
     /**
      * Sets a player's username.
+     *
      * @param newUsername the new username of the player.
      */
-    public void updateUsername(int id, String newUsername) throws SQLException{
+    public void updateUsername(int id, String newUsername) throws SQLException {
         this.username = newUsername;
         try {
             PlayerManager.updateAttribute(id, "username", newUsername);
@@ -282,19 +291,11 @@ public class Profile {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
     }
-
-    // ChatGPT was used to generate the original code for the following 2 methods in ProfileCreationTest.java.
-    // The prompt used was: How can I make it so every time this test is run, a new username email and password are generated and it's id is used to identify it in the assertEquals statement
-    // ChatGPT suggested using UUID.randomUUID() to generate a unique identifier for the username, email, and password.
-    // UUID.randomUUID() generates a random universally unique identifier using a strong pseudo random number generator.
-    // The identifier is then converted to a string using .toString. This ensures every time this test is ran, a unique profile is created.
-    // The rest of the code in this test case (ProfileCreation.createNewProfile and following) was written without any AI assistance.
-    // To ensure this code is efficiently reusable, I've moved the generation of usernames and passwords to their own methods in the Profile class.
 
     /**
      * Generates a unique username.
+     *
      * @return a unique username.
      */
     public static String generateUsername() {
@@ -304,6 +305,7 @@ public class Profile {
 
     /**
      * Generates a unique password.
+     *
      * @return a unique password.
      */
     public static String generatePassword() {
@@ -324,7 +326,26 @@ public class Profile {
         return playerRanking;
     }
 
-    //    public TTTRanking getTTTRanking() {
+
+    public static void main(String[] args) {
+        //int id = PlayerManager.registerPlayer("jake2", "jake2@email.com", "1263876");
+//        System.out.println(PlayerManager.updateAttribute(158,"wins_checkers","10"));
+//        System.out.println(ProfileDatabaseAccess.obtainProfile(158).getPlayerRanking().getWins(PlayerRanking.CHECKERS_INDEX));
+    }
+
+}
+
+
+// ChatGPT was used to generate the original code for the following 2 methods in ProfileCreationTest.java.
+// The prompt used was: How can I make it so every time this test is run, a new username email and password are generated and it's id is used to identify it in the assertEquals statement
+// ChatGPT suggested using UUID.randomUUID() to generate a unique identifier for the username, email, and password.
+// UUID.randomUUID() generates a random universally unique identifier using a strong pseudo random number generator.
+// The identifier is then converted to a string using .toString. This ensures every time this test is ran, a unique profile is created.
+// The rest of the code in this test case (ProfileCreation.createNewProfile and following) was written without any AI assistance.
+// To ensure this code is efficiently reusable, I've moved the generation of usernames and passwords to their own methods in the Profile class.
+
+
+//    public TTTRanking getTTTRanking() {
 //        return TTTRanking;
 //    }
 //
@@ -347,14 +368,3 @@ public class Profile {
 //    public void setCheckersRanking(CheckersRanking checkersRanking) {
 //        this.checkersRanking = checkersRanking;
 //    }
-
-    public static void main(String[] args) {
-        //int id = PlayerManager.registerPlayer("jake2", "jake2@email.com", "1263876");
-//        System.out.println(PlayerManager.updateAttribute(158,"wins_checkers","10"));
-//        System.out.println(ProfileDatabaseAccess.obtainProfile(158).getPlayerRanking().getWins(PlayerRanking.CHECKERS_INDEX));
-    }
-
-    public int getID() {
-        return id;
-    }
-}
