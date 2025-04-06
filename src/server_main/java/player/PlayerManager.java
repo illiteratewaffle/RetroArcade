387675ca01051b49a54a1d3935c2830cdb47f62a
10137257 <cristian.otalora1@ucalgary.ca>
2025-04-06 13:59:56 -0600

    Pushed addToFriendsRequestList method to server branch

--- src/server_main/java/player/PlayerManager.java
+++ src/server_main/java/player/PlayerManager.java
@@ -264,6 +264,23 @@ public class PlayerManager {
         }
     }
 
+    public static String addToFriendRequestList(int id, int newFriendId) {
+        String query = "UPDATE profiles SET friends = array_append(friend_requests, ?) WHERE id = ?";
+        try (PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setInt(1, newFriendId);
+            statement.setInt(2, id);
+
+            int rowsUpdated = statement.executeUpdate();
+            if (rowsUpdated > 0) {
+                return "added player " + newFriendId + " to player " + id + " Friend Request list";
+            } else {
+                return "No player found with ID: " + id;
+            }
+        } catch (SQLException e) {
+            return "Error updating 'friend_requests' array: " + e.getMessage();
+        }
+    }
+
     public static void main(String[] args) throws SQLException {
         getAttribute(22, "nickname");
     }
