fa645ebc1ee53038974b71298415935e6f30d2dc
nathania <nathania.castillo@ucalgary.ca>
2025-04-07 16:33:28 -0600

    Added login application and worked again on login.fxml and controller class

--- src/client_main/java/GUI_client/LoginGUIController.java
+++ src/client_main/java/GUI_client/LoginGUIController.java
@@ -1,11 +1,15 @@
 package GUI_client;
 
+import AuthenticationAndProfile.Authentication;
 import javafx.fxml.FXML;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -32,17 +36,32 @@ public class LoginGUIController {
      checkLogin();
     }
 
+    public void userSignUp() throws IOException {
+     signUp();
+    }
+    //for signUp pop
+    private void signUp() throws IOException {
+     //load sign up stuff here ltr
+     String username = this.username.getText();
+     String password = this.password.getText();
+     System.out.println("Sign-up button clicked");
+     wrongLogIn.setText("Sign-up button clicked");
+
+    }
     //login button sends to authentication
     private void checkLogin() throws IOException {
      String username = this.username.getText();
      String password = this.password.getText();
 
+     if(username.isEmpty() || password.isEmpty()) {
+      wrongLogIn.setText("enter username and password");
+      return;
+     }
+     boolean loginSuccess = Authentication.logIn(username, password);
+     //add pop up to switch scene if succesful and then if it is not succesful another pop up will show up with ok
 
-    }
-    //for signUp pop
-    private void signUp() throws IOException {
-     String username = this.username.getText();
-     String password = this.password.getText();
+     //tyler switch scene plsssssssssssssssssssssssssssssssssssssssssssssss
+
 
     }
 
