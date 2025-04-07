ea64c8e5d9cfe8841e4859b48e0e5dcb7856bb99
nathania <nathania.castillo@ucalgary.ca>
2025-04-07 13:46:07 -0600

    Updated Login Controller class

--- src/client_main/java/GUI_client/LoginGUIController.java
+++ src/client_main/java/GUI_client/LoginGUIController.java
@@ -2,9 +2,12 @@ package GUI_client;
 
 import javafx.fxml.FXML;
 
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.PasswordField;
+
+import java.io.IOException;
 
 
 public class LoginGUIController {
@@ -21,10 +24,27 @@ public class LoginGUIController {
     @FXML
     private PasswordField password;
 
+    //declares the label for login when either username or password is wrong
+    @FXML
+    private Label wrongLogIn;
+
+    public void userLogin() throws IOException {
+     checkLogin();
+    }
 
     //login button sends to authentication
+    private void checkLogin() throws IOException {
+     String username = this.username.getText();
+     String password = this.password.getText();
 
 
+    }
+    //for signUp pop
+    private void signUp() throws IOException {
+     String username = this.username.getText();
+     String password = this.password.getText();
 
-    //else signup-->
+    }
+
+
 }
