0e539a589d937f8ae44e6c17e94a995fa009ac1f
30208300 <colby.campbell@ucalgary.ca>
2025-04-09 18:38:50 -0600

    Supporting logging in, registering, and listening to the server.

--- src/server_main/java/client/ClientV2.java
+++ src/server_main/java/client/ClientV2.java
@@ -7,25 +7,54 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-import static client.TemporaryClient.outputToServer;
+import static management.JsonConverter.fromJson;
 import static management.JsonConverter.toJson;
 
 public class ClientV2 {
     private static BufferedReader input;
     private static PrintWriter output;
+    private static ConcurrentLinkedQueue<Map<String, Object>> messages = new ConcurrentLinkedQueue<>();
     private static boolean running = true;
 
-    // Make a shit ton of static functions, have a login
-    public static void login(String serverAddress, int serverPort, String username, String password) {
+    /**
+     * Login to the server
+     * @param serverAddress the server ip address
+     * @param serverPort the server port
+     * @param username the username of the player
+     * @param password the password of the player
+     * @return boolean true if successful, false if not
+     */
+    public static boolean login(String serverAddress, int serverPort, String username, String password) {
         // Make a Thread for input to server
-        Thread thread = Thread.ofVirtual().start(() -> listenServer(serverAddress, serverPort, username, password));
+        try (Socket socket = new Socket(serverAddress, serverPort)) {
+            // Create input and output streams for communication
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(socket.getOutputStream(), true);
+            // Authenticate the user before creating a thread
+            authenticate(username, password, null);
+            Thread thread = Thread.ofVirtual().start(() -> listenServer(socket));
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     // Make a shit ton of static functions, have a login
-    public static void register(String serverAddress, int serverPort, String username, String password, String email) {
+    public static boolean register(String serverAddress, int serverPort, String username, String password, String email) {
         // Make a Thread for input to server
-        Thread thread = Thread.ofVirtual().start(() -> listenServer(serverAddress, serverPort, username, password, email));
+        try (Socket socket = new Socket(serverAddress, serverPort)) {
+            // Create input and output streams for communication
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(socket.getOutputStream(), true);
+            // Authenticate the user before creating a thread
+            authenticate(username, password, email);
+            Thread thread = Thread.ofVirtual().start(() -> listenServer(socket));
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     public static void logout() {
@@ -33,37 +62,42 @@ public class ClientV2 {
         running = false;
     }
 
+    private static void authenticate(String username, String password, String email) {
+        // Authenticate user
+        Map<String, Object> authData = new HashMap<>();
+        if (email != null) {
+            authData.put("type", "login");
+            authData.put("email", email);
+        } else {
+            authData.put("type", "register");
+        }
+        authData.put("username", username);
+        authData.put("password", password);
+        // Send authData to the server to authenticate the user
+        output.println(toJson(authData));
+    }
+
     /**
      * This function is for listening to the server
-     * @param serverAddress the ip address for the server
-     * @param serverPort the port for the server
+     * @param socket the Socket for the connection
      */
-    private static void listenServer(String serverAddress, int serverPort, String username, String password, String email) {
-        try (Socket socket = new Socket(serverAddress, serverPort)) {
-            // Create input and output streams for communication
-            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            output = new PrintWriter(socket.getOutputStream(), true);
-            // Authenticate user
-            Map<String, Object> authData = new HashMap<>();
-            if (email != null) {
-                authData.put("type", "login");
-                authData.put("email", email);
-            } else {
-                authData.put("type", "register");
-            }
-            authData.put("username", username);
-            authData.put("password", password);
-            // Send authData to the server to authenticate the user
-            output.println(toJson(authData));
-            // Now listen to all inputs from the server
-            while (running) {
+    private static void listenServer(Socket socket) {
+        // Now listen to all inputs from the server
+        while (running) {
+            try {
+                // Take a response and put it in the ConcurrentLinkedQueue
                 String response = input.readLine();
-                System.out.println(response);
-                // TODO: Take response and put it in some kind of thread safe data type
+                messages.add(fromJson(response));
+                messages.notifyAll();
+                // TODO: TEMPORARY PRINT TO SHOW IT WAS RECEIVED
+                System.out.println("Received message: " + response);
+            } catch (IOException e) {
+                System.out.println("Server may have shut down?: " + e.getMessage());
+                // Shut down thread if IOException
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid json message received: " + e.getMessage());
             }
-        } catch (IOException e) {
-            // TODO: RETURN A FALSE TO START SERVER MAYBE?
-            System.out.println("Unable to connect to server");
         }
     }
 }
