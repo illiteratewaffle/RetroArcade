b65716c31786d9094af3dec100b339d91c955bc7
30213188 <ashton.giles@ucalgary.ca>
2025-04-10 23:35:54 -0600

    added documentation to checkers highlight board class

--- src/client_main/java/GameLogic_Client/Checkers/CheckersHighlightBoard.java
+++ src/client_main/java/GameLogic_Client/Checkers/CheckersHighlightBoard.java
@@ -1,13 +1,21 @@
 package GameLogic_Client.Checkers;
 
+// importing the abstract board and ivec2 classes
 import GameLogic_Client.AbstractBoard;
 import GameLogic_Client.Ivec2;
 
 
 public class CheckersHighlightBoard extends AbstractBoard
 {
+    /**
+     * constructor for the highlight board which takes the number of rows and columns in the board as well as the board itself
+     * @param rows the number of rows
+     * @param cols the number of columns
+     * @param board the 2d board array
+     */
     public CheckersHighlightBoard(int rows, int cols, int[][] board)
     {
+        // instantiate the super class
         super(rows, cols, board);
     }
 
@@ -19,6 +27,7 @@ public class CheckersHighlightBoard extends AbstractBoard
      */
     public void markTile(Ivec2 tileCoordinate, CheckersHighlightType highlight)
     {
+        // call the set piece method passing in the cord and either 0 or the highlight value based on if highlight is null
         setPiece(tileCoordinate, (highlight == null) ? 0 : highlight.getValue());
     }
 }
