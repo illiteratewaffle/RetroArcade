205ebdc771e3b0cd34666036504f5238ae02a74d
30213188 <ashton.giles@ucalgary.ca>
2025-04-10 21:26:05 -0600

    added client classes

--- src/client_main/java/client/C4Client.java
+++ src/client_main/java/client/C4Client.java
@@ -1,29 +1,29 @@
-//package client;
-//
-//import java.util.HashMap;
-//
-//public class C4Client {
-//    public void receiveInput(GameLogic_Client.Ivec2 input){
-//    String col = String.valueOf(input.x);
-//    HashMap<String, Object> data = new HashMap<>();
-//    data.put("type", "game-move");
-//    data.put("command", "receiveInput");
-//    data.put("game","Connect4");
-//    data.put("Column", col);
-//    Client.networkingMethod(data);
-//    }
-//    public void getCurrentPlayer(){
-//        HashMap<String, Object> data = new HashMap<>();
-//        data.put("type", "game-move");
-//        data.put("command", "getCurrentPlayer");
-//        data.put("game","Connect4");
-//        Client.networkingMethod(data);
-//    }
-//    public void sendmessage(String message){
-//        HashMap<String, Object> data = new HashMap<>();
-//        data.put("type", "chat");
-//        data.put("message", message);
-//        data.put("game","Connect4");
-//        Client.networkingMethod(data);
-//    }
-//}
+package client;
+
+import java.util.HashMap;
+
+public class C4Client {
+    public void receiveInput(GameLogic_Client.Ivec2 input){
+        String col = String.valueOf(input.x);
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("type", "game-move");
+        data.put("command", "receiveInput");
+        data.put("game","Connect4");
+        data.put("Column", col);
+        Client.networkingMethod(data);
+    }
+    public void getCurrentPlayer(){
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("type", "game-move");
+        data.put("command", "getCurrentPlayer");
+        data.put("game","Connect4");
+        Client.networkingMethod(data);
+    }
+    public void sendmessage(String message){
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("type", "chat");
+        data.put("message", message);
+        data.put("game","Connect4");
+        Client.networkingMethod(data);
+    }
+}
