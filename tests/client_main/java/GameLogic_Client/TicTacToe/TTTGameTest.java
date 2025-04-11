68a3596fff03501bb43a463a76a83e6c56a979a1
Emma_Djukic <emma.djukic@ucalgary.ca>
2025-04-10 18:22:03 -0600

    Fixed TTTGame tests

--- tests/client_main/java/GameLogic_Client/TicTacToe/TTTGameTest.java
+++ tests/client_main/java/GameLogic_Client/TicTacToe/TTTGameTest.java
@@ -1,49 +1,113 @@
 package client_main.java.GameLogic_Client.TicTacToe;
+
 /**
  * TTGameTest class to test the functions and methods of the TTTGame class.
- * Author: Vicente David - Game Logic Team
+ * Author: Vicente David, Emma Djukic - Game Logic Team
  */
 
-import GameLogic_Client.TicTacToe.TTTBoard;
+import GameLogic_Client.GameState;
+import GameLogic_Client.Ivec2;
 import GameLogic_Client.TicTacToe.TTTGame;
 import GameLogic_Client.TicTacToe.TTTPiece;
-import GameLogic_Client.Ivec2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-class TTTGameTest {
+
+public class TTTGameTest {
+
     private TTTGame game;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         game = new TTTGame();
     }
 
     @Test
-    void makeMove() {
-        assertTrue(game.makeMove(0, 0), "Move should be valid as the board is empty");
-        assertFalse(game.makeMove(0, 0), "Move should be invalid as this tile is not empty");
+    public void testGameInitialization() {
+        assertEquals(1, game.currentPlayer);
+        assertEquals(GameState.ONGOING, game.gameState);
+        assertNotNull(game.board);
     }
 
     @Test
-    void switchTurn() {
-        assertEquals(1, game.currentPlayer, "Current player should be 1 at the start of the game");
-        game.switchTurn();
-        assertEquals(2, game.currentPlayer, "Current player should be 2 after first turn ends");
-        game.switchTurn();
-        assertEquals(1, game.currentPlayer, "Current player should be 1 after player 2 ends turn");
+    public void testValidMoveAndSwitchTurn() {
+        assertTrue(game.makeMove(0, 0));
+        assertEquals(2, game.currentPlayer);
     }
 
     @Test
-    void checkWin() {
+    public void testInvalidMoveOccupiedCell() {
+        assertTrue(game.makeMove(0, 0));
+        assertFalse(game.makeMove(0, 0)); // same cell
+    }
+
+    @Test
+    public void testWinningMoveRow() {
+        game.makeMove(0, 0); // X
+        game.makeMove(1, 0); // O
+        game.makeMove(0, 1); // X
+        game.makeMove(1, 1); // O
+        game.makeMove(0, 2); // X - win
+
+        assertEquals(GameState.P1WIN, game.gameState);
+        assertEquals(1, game.checkWin(game.board));
+        assertTrue(game.isGameOver(game.board));
+    }
+
+    @Test
+    public void testWinningMoveDiagonal() {
+        game.makeMove(0, 0); // X
+        game.makeMove(0, 1); // O
+        game.makeMove(1, 1); // X
+        game.makeMove(2, 1); // O
+        game.makeMove(2, 2); // X - win
+
+        assertEquals(GameState.P1WIN, game.gameState);
+        assertEquals(1, game.checkWin(game.board));
+    }
+
+    @Test
+    public void testDrawGame() {
+        game.makeMove(0, 0); // X
+        game.makeMove(0, 1); // O
+        game.makeMove(0, 2); // X
+        game.makeMove(1, 1); // O
+        game.makeMove(1, 0); // X
+        game.makeMove(1, 2); // O
+        game.makeMove(2, 1); // X
+        game.makeMove(2, 0); // O
+        game.makeMove(2, 2); // X
+
+        assertEquals(GameState.TIE, game.gameState);
+        assertTrue(game.checkDraw(game.board));
+        assertTrue(game.isGameOver(game.board));
+    }
+
+    @Test
+    void getPiece() {
+        Ivec2 point = new Ivec2(0, 0);
+        assertEquals(TTTPiece.EMPTY, game.getPiece(point), "Should get the empty piece as none have been placed");
+
         game.makeMove(0, 0);
-        game.makeMove(1,0);
-        game.makeMove(0,1);
-        assertFalse(game.checkWin(game.board), "should return false as no player has achieved a win condition");
-        game.makeMove(1,1);
-        game.makeMove(0,2);
-        assertTrue(game.checkWin(game.board), "should return true as player 3 has a tile in the first row");
+        assertEquals(TTTPiece.X, game.getPiece(point), "Should get 'X' in top left tile");
+
+        Ivec2 point2 = new Ivec2(0, 1);
+        game.makeMove(1, 0);
+        assertEquals(TTTPiece.O, game.getPiece(point2), "Should get 'O' in top middle tile");
+    }
+
+    @Test
+    public void testCheckWinNoWinner() {
+        game.makeMove(0, 0); // X
+        game.makeMove(1, 1); // O
+        assertEquals(0, game.checkWin(game.board));
+    }
+
+    @Test
+    public void testCheckDrawFalseIfBoardNotFull() {
+        game.makeMove(0, 0);
+        assertFalse(game.checkDraw(game.board));
     }
 
     @Test
@@ -62,18 +126,13 @@ class TTTGameTest {
     }
 
     @Test
-    void getPiece() {
-        Ivec2 point = new Ivec2(0, 0);
-        assertEquals(TTTPiece.EMPTY, game.getPiece(point), "Should get the empty piece as none have been placed");
-
+    public void testSwitchTurnTwice() {
         game.makeMove(0, 0);
-        assertEquals(TTTPiece.X, game.getPiece(point), "Should get 'X' in top left tile");
-
-        Ivec2 point2 = new Ivec2(0, 1);
-        game.makeMove(1, 0);
-        assertEquals(TTTPiece.O, game.getPiece(point2), "Should get 'O' in top middle tile");
+        assertEquals(2, game.currentPlayer);
+        game.makeMove(1, 1);
+        assertEquals(1, game.currentPlayer);
     }
-    
+
     @Test
     void isGameOver() {
         // NOTE: these tests are technically unnecessary and redundant as the isGameOver() function
